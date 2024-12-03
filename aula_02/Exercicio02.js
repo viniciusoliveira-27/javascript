da4f31b5a0f5b78a24a94c286c5cadce67a49610
Vinicius Ribeiro <viniciusoliveira.270304@gmail.com>
2024-12-03 15:18:02 -0300

    colocando condicional  no comando

--- aula_02/Exercicio02.js
+++ aula_02/Exercicio02.js
@@ -7,6 +7,16 @@ nota02 = leia.questionFloat("Nota 2 = ")
 nota03 = leia.questionFloat("Nota 3 = ")
 nota04 = leia.questionFloat("Nota 4 = ")
 
+
 mediaFinal = (nota01 + nota02 + nota03 + nota04) / 4;
 
 console.log("Média final: ", mediaFinal.toFixed(1))
+
+if(mediaFinal <= 4 ) {
+    console.log("Reprovado")
+} else if (mediaFinal <= 5.5) {
+    console.log("Exame, estude mais!")
+} else {
+    console.log("Aprovado")
+}
+
