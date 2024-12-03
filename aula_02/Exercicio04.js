a0095a7694706ff770ae1167bc100075b1930aa1
Vinicius Ribeiro <viniciusoliveira.270304@gmail.com>
2024-12-03 16:16:11 -0300

    correcao exercicio 4

--- aula_02/Exercicio04.js
+++ aula_02/Exercicio04.js
@@ -1,14 +1,13 @@
 const leia = require("readline-sync")
 
-let numero01, numero02, numero03, numero04, produto1, produto2;
+let numero01, numero02, numero03, numero04, diferenca;
 
 numero01 = leia.questionFloat("Numero 01: ")
 numero02 = leia.questionFloat("Numero 02: ")
 numero03 = leia.questionFloat("Numero 03: ")
 numero04 = leia.questionFloat("Numero 04: ")
 
-produto1 = numero01 * numero02
-produto2 = numero03 * numero04
+diferenca = (numero01 * numero02) / (numero03 * numero04)
 
-console.log("Diferença: ", produto1.toFixed(1) - produto2.toFixed(1))
+console.log("Diferença: ", diferenca)
 
