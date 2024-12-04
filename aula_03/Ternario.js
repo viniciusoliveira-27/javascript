e6f332ecdb8732a9fbeced62f8625d62595af087
Vinicius Ribeiro <viniciusoliveira.270304@gmail.com>
2024-12-04 11:41:34 -0300

    If Ternario

---
+++ aula_03/Ternario.js
@@ -0,0 +1,5 @@
+let idade = 17;
+
+// if Ternário (um if simplificado usado apenas pra um if e else)
+
+idade > 18 ? console.log("Cliente Maior de 18 anos") : console.log("Cliente Menor de 18 anos")
