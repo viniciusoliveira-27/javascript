4b64bd6f72d9b926ee9a5bf62ee3aed79a811229
Vinicius Ribeiro <viniciusoliveira.270304@gmail.com>
2024-12-05 16:28:04 -0300

    terminando exercicio 4

--- aula_03/exercicios_condicionais_If/Exercicio04.js
+++ aula_03/exercicios_condicionais_If/Exercicio04.js
@@ -6,9 +6,9 @@ palavras digitadas pelo usuário:
 */
 const leia = require("readline-sync");
 
-let animal = leia.question("Digite se o animal é vertebrado ou invertebrado: ");
-let grupo = leia.question("Digite em qual grupo esse animal pertence (ave, mamifero, inseto, anelideo): ");
-let dieta = leia.question("Digite a dieta do animal (carnivoro, onivoro, herbivoro, hematofago): ");
+let animal = leia.question("\nDigite se o animal é vertebrado ou invertebrado: ");
+let grupo = leia.question("\nDigite em qual grupo esse animal pertence (ave, mamifero, inseto, anelideo): ");
+let dieta = leia.question("\nDigite a dieta do animal (carnivoro, onivoro, herbivoro, hematofago): ");
 
 let resultadoAnimal;
 
@@ -16,16 +16,41 @@ if(animal === "vertebrado" ) {
     if (grupo === "ave") {
         if (dieta === "carnivoro") {
             resultadoAnimal = "Aguia"
-        } else {
+        } else if (dieta === "onivoro") {
             resultadoAnimal = "Pomba"
+        } else {
+            console.log("Não existe uma ave com essa dieta ", dieta)
+            process.exit();
         }
     } else if (grupo === "mamifero") {
         if (dieta === "onivoro") {
             resultadoAnimal = "Homem"
+        } else if (dieta === "herbivoro") {
+            resultadoAnimal = "Vaca"            
         } else {
-            resultadoAnimal = "Vaca"
-            
+            console.log("Não existe uma mamifero com essa dieta ", dieta)
+            process.exit();
         }
-
+    }
+} else if (animal === "invertebrado") {
+    if (grupo === "inseto") {
+        if (dieta === "hematofago") {
+            resultadoAnimal = "Pulga"
+        } else if (dieta === "herbivoro") {
+            resultadoAnimal = "Lagarta"
+        } else {
+            console.log("Não existe um inseto com essa dieta ", dieta)
+            process.exit();
+        }
+    } else if (grupo === "anelideo") {
+        if (dieta === "hematofago") {
+            resultadoAnimal = "Sanguessuga"
+        } else if (dieta === "Onivoro") {
+            resultadoAnimal === "Minhoca"
+        } else {
+            console.log("Não existe um Anelideo com essa dieta ", dieta)
+            process.exit();
+        }
     }
 }
+console.log("\n",resultadoAnimal);
