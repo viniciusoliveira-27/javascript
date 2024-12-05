600e796527104e5e66a91434e8987fd9952a395a
Vinicius Ribeiro <viniciusoliveira.270304@gmail.com>
2024-12-04 21:58:53 -0300

    arrumando o enunciado das perguntas

--- aula_03/exercicios_condicionais_switch/Exercicio05.js
+++ aula_03/exercicios_condicionais_switch/Exercicio05.js
@@ -1,5 +1,7 @@
 /*
-05 - Com base na tabela abaixo, escreva um algoritmo que leia o código de um item (número inteiro entre 1 e 6) e a quantidade comprada deste item (número inteiro). A seguir, mostre na tela o valor total da conta e o nome do produto que foi comprado.
+05 - Com base na tabela abaixo, escreva um algoritmo que leia o código de um item
+ (número inteiro entre 1 e 6) e a quantidade comprada deste item (número inteiro). A 
+ seguir, mostre na tela o valor total da conta e o nome do produto que foi comprado.
 */
 const leia = require("readline-sync")
 
--- aula_03/exercicios_condicionais_switch/Exercicio06.js
+++ aula_03/exercicios_condicionais_switch/Exercicio06.js
@@ -1,5 +1,8 @@
 /*
-06 - Com base na tabela abaixo, escreva um algoritmo que leia o Nome do Colaborador (string), o Código do Cargo do Colaborador (número inteiro entre 1 e 6) e o Salário (número real). A seguir, mostre na tela o Nome do Colaborador, o Cargo e o novo Salário reajustado.
+06 - Com base na tabela abaixo, escreva um algoritmo que leia o Nome do Colaborador
+ (string), o Código do Cargo do Colaborador (número inteiro entre 1 e 6) e o Salário
+  (número real). A seguir, mostre na tela o Nome do Colaborador, o Cargo e o novo
+   Salário reajustado.
 */
 
 const leia = require("readline-sync")
